2440d96cae60ceaefc25b4c468e78470dd4b6c73
wangfiox <wangfiox@gmail.com>
2025-12-31 01:49:11 +0800

    disassemble

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -22,6 +22,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
   assert(0);
 }
 
+// 这些只是骗过编译器的语法检测, 实际上啥也没干
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   assert(0);
 }
