dc23c9daba38db8e3fad921254f715ea8f206ded
wangfiox <wangfiox@gmail.com>
2026-01-07 19:14:21 +0800

    重构am

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -1,14 +1,11 @@
-AM_SRCS := riscv/npc/start.S \
-           riscv/npc/trm.c \
-           riscv/npc/ioe.c \
-           riscv/npc/timer.c \
-           riscv/npc/input.c \
-           riscv/npc/cte.c \
-           riscv/npc/trap.S \
-           platform/dummy/vme.c \
-           platform/dummy/mpe.c
+AM_SRCS := platform/npc/trm.c \
+           platform/npc/ioe/ioe.c \
+           platform/npc/ioe/timer.c \
+           platform/npc/ioe/input.c \
+           platform/npc/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
+CFLAGS    += -I$(AM_HOME)/am/src/platform/npc/include
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
