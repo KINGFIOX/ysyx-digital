848c0e124b3d79afba3178bc11de853033072906
wangfiox <wangfiox@gmail.com>
2026-01-15 23:38:58 +0800

    修复warning

--- npc/playground/src/NpcCore.scala
+++ npc/playground/src/NpcCore.scala
@@ -159,6 +159,8 @@ class NpcCoreTop extends Module with HasCoreParameter with HasRegFileParameter {
 object NpcCoreTop extends App {
   val firtoolOptions = Array(
     "--lowering-options=" + List(
+      // make yosys happy
+      // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
       "disallowLocalVariables",
       "disallowPackedArrays",
       "locationInfoStyle=wrapInAtSquareBracket"
